4525ed2009f9b3ccbdf20d34df36605b8b7ab2f3
Aniketkaygude <aniketkaygude2003@gmail.com>
2025-06-16 23:58:40 +0530

    removed todos

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ app.use(express.json());
 
 const arr = [
 
-    
+
   { name: "Aniket", age: 22, gender: "Male" },
   { name: "Riya", age: 25, gender: "Female" },
-  { name: "Aman", age: 28, gender: "Male" },
-  { name: "Sneha", age: 21, gender: "Female" },
-  { name: "Vikram", age: 30, gender: "Male" },
-  { name: "Pooja", age: 24, gender: "Female" },
-  { name: "Rohan", age: 26, gender: "Male" },
-  { name: "Kavya", age: 23, gender: "Female" },
-  { name: "Yash", age: 27, gender: "Male" },
-  { name: "Neha", age: 22, gender: "Female" },
-  { name: "Arjun", age: 29, gender: "Male" }
+//   { name: "Aman", age: 28, gender: "Male" },
+//   { name: "Sneha", age: 21, gender: "Female" },
+//   { name: "Vikram", age: 30, gender: "Male" },
+//   { name: "Pooja", age: 24, gender: "Female" },
+//   { name: "Rohan", age: 26, gender: "Male" },
+//   { name: "Kavya", age: 23, gender: "Female" },
+//   { name: "Yash", age: 27, gender: "Male" },
+//   { name: "Neha", age: 22, gender: "Female" },
+//   { name: "Arjun", age: 29, gender: "Male" }
 ];
 
 app.get("/",(req,res)=>{
